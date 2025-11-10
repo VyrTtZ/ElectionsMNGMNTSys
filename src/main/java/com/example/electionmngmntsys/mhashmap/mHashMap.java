6ec6cb31e3b7c2f41fa57c0e4eb419513eede34e
VyrTtZ <tarkagustaw@gmail.com>
2025-11-10 18:51:29 -0100

    minor changes

--- src/main/java/com/example/electionmngmntsys/mhashmap/mHashMap.java
+++ src/main/java/com/example/electionmngmntsys/mhashmap/mHashMap.java
@@ -1,32 +1,15 @@
 package com.example.electionmngmntsys.mhashmap;
 
 import com.example.electionmngmntsys.mlinkedlist.mNodeL;
-//FIX THE HASH THINGIE
+//BLEHHH
 public class mHashMap<X, Y> {
     private mNodeH<X, Y> head;
     private int hash;
+    private final int INITIALCAPACITY = 100;
+    private int CURRENTCAPACITY = INITIALCAPACITY;
 
     public mHashMap(){
         head = null;
-    }
-
-    public void putNode(mNodeH<X, Y> n){
-        if(head == null){
-            head = n;
-        }
-        else{
-            mNodeH<X, Y> current = head;
-            while(current.next != null)
-                current = current.next;
-            current.next = n;
-        }
-    }
-    public void add(X x, Y y){
-        putNode(new mNodeH<>(x, y, 0, null));
-    }
-
-    public boolean isEmpty(){
-        return head == null;
     }
 
 
--- src/main/java/com/example/electionmngmntsys/mhashmap/mNodeH.java
+++ src/main/java/com/example/electionmngmntsys/mhashmap/mNodeH.java
@@ -3,14 +3,12 @@ package com.example.electionmngmntsys.mhashmap;
 public class mNodeH <X, Y>{
     X key;
     Y value;
-    int hash;
     mNodeH<X, Y> next;
 
-    public mNodeH(X k, Y v, int h, mNodeH<X, Y> n){
+    public mNodeH(X k, Y v){
         key = k;
         value = v;
-        hash = h;
-        next = n;
+        next = null;
 
     }
 
