4b0fd2bcc8464bb8519a7d15519846db0b67df80
joey0 <20112816@mail.wit.ie>
2025-11-24 17:02:50 +0000

    Established Test methods and created persistence package

---
+++ src/main/Test/electionmngmntsys/mhashmap/mHashMapTest.java
@@ -0,0 +1,43 @@
+package electionmngmntsys.mhashmap;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.io.Serializable;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class mHashMapTest {
+    private MyHashMap<String, Integer> map;
+
+    @BeforeEach
+    void setUp() {
+        map = new MyHashMap<>();
+    }
+
+
+
+    @Test
+    void putNode() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void containsKey() {
+    }
+
+    @Test
+    void containsValue() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void swapValues() {
+    }
+}
---
+++ src/main/Test/electionmngmntsys/mlinkedlist/mLinkedListTest.java
@@ -0,0 +1,28 @@
+package electionmngmntsys.mlinkedlist;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class mLinkedListTest {
+
+    @Test
+    void addNode() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void swapNodes() {
+    }
+
+    @Test
+    void testSwapNodes() {
+    }
+}
